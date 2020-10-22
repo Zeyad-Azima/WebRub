8117c957df85394547f8539965326ede3405939f
0ffsec <62406753+Zeyad-Azima@users.noreply.github.com>
2020-10-22 14:36:49 +0200

    Update brute.rb

--- core/brute.rb
+++ core/brute.rb
@@ -2,7 +2,7 @@
 #Author:    Zeyad Azima                                        #
 #Github:    https://github.com/Zeyad-Azima                     #
 #Facebook:  https://www.facebook.com/elkingzeyad.azeem         #
-#Version:   0.1                                                #
+#Version:   0.2                                                #
 # There is more updates soon..                                 #
 ################################################################
 #
