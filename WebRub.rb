c2774812ff1c67e88a24bd48404c65c6fb2e4094
0ffsec <62406753+Zeyad-Azima@users.noreply.github.com>
2020-10-24 16:10:48 +0200

    Update WebRub.rb

--- WebRub.rb
+++ WebRub.rb
@@ -2,17 +2,19 @@
 #Author:    Zeyad Azima                                        #
 #Github:    https://github.com/Zeyad-Azima                     #
 #Facebook:  https://www.facebook.com/elkingzeyad.azeem         #
-#Version:   0.2                                                #
+#Version:   0.3                                                #
 # There is more updates soon..                                 #
 ################################################################
 #
 require 'colorize'
-load 'core/live.rb'
-load 'core/resolver.rb'
-load 'core/subdomain.rb'
-load 'core/brute.rb'
-load 'core/wtweb.rb'
-load 'core/shodan.rb'
+load 'core/live_resolve/live.rb'
+load 'core/live_resolve/resolver.rb'
+load 'core/live_resolve/Domin_ip.rb'
+load 'core/sub_enum/subdomain.rb'
+load 'core/sub_enum/subenum.rb'
+load 'core/dirbrute/brute.rb'
+load 'core/ext/wtweb.rb'
+load 'core/ext/shodan.rb'
 
 $choice
 class Start
@@ -21,37 +23,49 @@ class Start
 
     puts """
 
-██╗    ██╗███████╗██████╗ ██████╗ ██╗   ██╗██████╗
-██║    ██║██╔════╝██╔══██╗██╔══██╗██║   ██║██╔══██╗
-██║ █╗ ██║█████╗  ██████╔╝██████╔╝██║   ██║██████╔╝
-██║███╗██║██╔══╝  ██╔══██╗██╔══██╗██║   ██║██╔══██╗
-╚███╔███╔╝███████╗██████╔╝██║  ██║╚██████╔╝██████╔╝
- ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═════╝
-                   ZeyadAzima
-          https://github.com/Zeyad-Azima
-===================================================
-|                    WebRub                       |
-===================================================
-| 1-Subdomain BruteForce (Fast)                   |
-| 2-live domain checker (Fast)                    |
-| 3-Resolve domains to ips (Fast)                 |
-| 4-Directory BruteForce (Fast)                   |
-| 5-Extract Web Headers (Fast)                    |
-| 6-Shodan Searcher (Fast)                        |
-===================================================
-|                     V0.2                        |
-|       there is more updates coming soon         |
-===================================================
+        ██╗    ██╗███████╗██████╗ ██████╗ ██╗   ██╗██████╗
+        ██║    ██║██╔════╝██╔══██╗██╔══██╗██║   ██║██╔══██╗
+        ██║ █╗ ██║█████╗  ██████╔╝██████╔╝██║   ██║██████╔╝
+        ██║███╗██║██╔══╝  ██╔══██╗██╔══██╗██║   ██║██╔══██╗
+        ╚███╔███╔╝███████╗██████╔╝██║  ██║╚██████╔╝██████╔╝
+        ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═════╝
+                           ZeyadAzima
+                 https://github.com/Zeyad-Azima
+=====================================================================
+|                            WebRub                                 |
+=====================================================================
+| 1-Subdomain Enumeration (Very Fast)  7-Extract Web Headers (Fast) |
+| 2-Subdomain BruteForce (Fast)        8-Shodan Searcher (Fast)     |
+| 3-live domain checker (Fast)                                      |
+| 4-Resolve domains to ips (Fast)                                   |
+| 5-Resolve ips to domains (Fast)                                   |
+| 6-Directory BruteForce (Fast)                                     |
+=====================================================================
+|                              V0.3                                 |
+=====================================================================
 
 """.red
 
     puts "Enter Your Choice(ex: 1,2,3,4,5,6):".red
     $choice=gets.chomp.to_i
   end
+
   def main
 
 
     if $choice==1
+      puts "Subdomain Enumeration:>".green
+      puts "Enter your Target (ex: 'google.com'): ".green
+      target=gets.chomp.to_s
+      subenum=Subenum.new
+      subenum.attack(target)
+      puts ""
+      puts "[+] Results saved to #{target}.txt"
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+    elsif $choice==2
       puts "Subdomain BruteForce:>".green
       puts "Enter your Target (ex: 'google.com'): ".green
       domain=gets.chomp.to_s
@@ -63,7 +77,7 @@ class Start
       system('clear') || system('cls')
 
 
-    elsif $choice==2
+    elsif $choice==3
       puts "live domain checker:>".green
       puts "Enter subdomains list directory(ex: /home/root/subdomains.txt):".green
       list=gets.chomp
@@ -75,7 +89,7 @@ class Start
       system('clear') || system('cls')
 
 
-    elsif $choice==3
+    elsif $choice==4
       puts "Resolve domains to ips:>".green
       puts "Enter subdomains list directory(ex: /home/root/subdomains.txt):".green
       s_list=gets.chomp
@@ -86,8 +100,18 @@ class Start
       cont=gets.chomp
       system('clear') || system('cls')
 
+    elsif $choice==5
+      puts "Resolve ips to domains:>".green
+      puts "Enter IPS list directory(ex: /home/root/IPS.txt):".green
+      i_list=gets.chomp.to_s
+      ip=Domain.new
+      ip.ips(i_list)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
 
-    elsif $choice==4
+    elsif $choice==6
       puts "Directory BruteForce:>".green
       puts "Enter your Target Domain or Domain with Directory(ex: 'google.com' or a directory 'google.com/profiles'): ".green
       host=gets.chomp.to_s
@@ -102,7 +126,7 @@ class Start
       cont=gets.chomp
       system('clear') || system('cls')
 
-    elsif $choice==5
+    elsif $choice==7
       puts ""
       puts "Extract Web Headers:>".green
       puts "Enter subdomains list(ex: subdomains.txt): ".green
@@ -115,7 +139,7 @@ class Start
       cont=gets.chomp
       system('clear') || system('cls')
 
-    elsif $choice==6
+    elsif $choice==8
       puts ""
       puts "Shodan Searcher:>".green
       puts "Enter Your target doamin(ex: google.com):".green
@@ -126,6 +150,7 @@ class Start
       puts "[+] Press Enter to Continue: ".blue
       cont=gets.chomp
       system('clear') || system('cls')
+
 
     end
   end
